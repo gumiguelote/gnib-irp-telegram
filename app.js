1a273fc811a7fb27216e80afcd8cdd05f92abb1e
Gustavo Miguelote <gustavomiguelote@me.com>
2021-11-09 00:38:17 -0300

    feat(commands): Adding commands to show informations

--- app.js
+++ app.js
@@ -5,42 +5,86 @@ const TelegramBot = require("node-telegram-bot-api");
 const express = require("express");
 const axios = require("axios");
 const app = express();
-const port = process.env.PORT;
-const token = process.env.TELEGRAM_TOKEN;
-const bot = new TelegramBot(token, { polling: true });
+const PORT = process.env.PORT;
+const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
+const INTERVAL_REQUEST_IN_SECONDS = process.env.INTERVAL_REQUEST_IN_SECONDS;
+const CHAT_ID = process.env.CHAT_ID;
+const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
+var content = "";
 
-var chatId = "";
-
-bot.on("message", (msg) => {
-  chatId = msg.chat.id;
+app.listen(PORT, () => {
+  // checkAppointments();
+  startCommandsBotEvent();
 });
 
-app.listen(port, () => {
-  checkAppointments();
+app.get("/", (req, res) => {
+  res.send(`<script>
+  setTimeout(() => {
+    window.location.reload()
+  }, 5000)
+  </script> ${content}`);
 });
 
 const checkAppointments = () => {
   setInterval(async () => {
-    if (chatId) {
-      try {
-        const { data } = await axios.get(
-          `${process.env.APPOINTMENTS_ENDPOINT}&_=${Date.now()}`
-        );
-        const isEmpty = JSON.stringify(data) === '{"empty":"TRUE"}';
-        if (!isEmpty) {
-          bot.sendMessage(
-            chatId,
-            `Agendamento encontrado!
-            Corre clicar no link
+    try {
+      const { data } = await axios.get(`${process.env.APPOINTMENTS_ENDPOINT}`);
+      const isEmpty = JSON.stringify(data) === '{"empty":"TRUE"}';
+      if (!isEmpty) {
+        content += `${returnTimeStamp()} - 
+          <a 
+            href="https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/AppSelect?OpenForm">
+            Appointment Found!
+          </a>
+        <br>`;
+        bot.sendMessage(
+          CHAT_ID,
+          ` Appointment Found!
             https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/AppSelect?OpenForm
             `
-          );
-        }
-        console.log("...OK...");
-      } catch (error) {
-        console.error(error);
-        bot.sendMessage(chatId, `${error}`);
+        );
       }
+      content += `Request -> ${returnTimeStamp()} - Nothing found <br>`;
+    } catch (error) {
+      content += `Request -> ${returnTimeStamp()} - ${error} <br>`;
+      bot.sendMessage(CHAT_ID, `${error}`);
     }
-  }, 30000);
+  }, INTERVAL_REQUEST_IN_SECONDS * 1000);
+};
+
+const returnTimeStamp = () => {
+  return `${new Date().toLocaleDateString()} - ${new Date().toLocaleTimeString()}`;
+};
+
+const startCommandsBotEvent = () => {
+  bot.onText(/\/start (.+)/, (msg, match) => {
+    const command = match[1];
+
+    if (command === "myid") {
+      bot.sendMessage(CHAT_ID, msg.chat.id);
+    }
+
+    if (command === "viewcontent") {
+      bot.sendMessage(CHAT_ID, content);
+    }
+
+    if (command === "clearcontent") {
+      bot.sendMessage(CHAT_ID, "Cleared");
+    }
+
+    if (command === "irpsite") {
+      bot.sendMessage(
+        CHAT_ID,
+        "https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/AppSelect?OpenForm"
+      );
+    }
+  });
+
+  bot.onText(/\/help/, (msg, match) => {
+    const commands = `
+/start myid  -> Show the chat_id
+/start viewcontent -> Show log
+/start clearcontent -> Clear log from memory`;
+    bot.sendMessage(CHAT_ID, commands);
+  });
 };
